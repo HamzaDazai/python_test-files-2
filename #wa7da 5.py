ffc8155ddcd9a672382372dc105ed2b528b057d4
Hamza Dazia <hamzadazai@gmail.com>
2024-02-22 16:00:52 +0100

    new files test

---
+++ #wa7da 5.py
@@ -0,0 +1,6 @@
+#wa7da 5
+#-----------------------------split----------------------------------
+name_string = input("Enter names separated by a comma .. ")# hana kalt user i kteb names ma fsolin bi ,
+names = name_string.split(", ")#split : hadi adat kat fsal bent ayh 7aja o khasik tkol leha shno heya l3alam li khat beyan lik adat fasl kifma 3adna hna heya fasila (, )
+print(names)
+print(len(names))
